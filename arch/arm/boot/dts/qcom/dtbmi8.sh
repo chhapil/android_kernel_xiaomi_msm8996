3c7b3898208db1a9d9f7dbe7701fcbffecbc4606
chhapil <ru2raj@gmail.com>
2016-11-05 18:45:43 +0530

    build files n fixes

---
+++ arch/arm/boot/dts/qcom/dtbmi8.sh
@@ -0,0 +1,3 @@
+cd /root/XiNK/kernel/arch/arm/boot/dts/qcom/
+cat *.dtb > alldtbcat
+cat /root/Downloads/Android-Kitchen-0.224/dtb/Image.gz /root/XiNK/kernel/arch/arm/boot/dts/qcom/alldtbcat > /root/Downloads/Android-Kitchen-0.224/dtb/zImage
